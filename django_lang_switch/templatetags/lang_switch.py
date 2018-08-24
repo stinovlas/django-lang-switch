df535a92c7df826d2a6fa93f2ef0f17ddea2ab3f
Jan Musílek <jan.musilek@nic.cz>
2018-08-24 11:37:30 +0200

    Add redirect_to arg to lang_switch_dropdown

--- django_lang_switch/templatetags/lang_switch.py
+++ django_lang_switch/templatetags/lang_switch.py
@@ -5,6 +5,15 @@ register = template.Library()
 
 
 @register.inclusion_tag('django_lang_switch/dropdown.html')
-def lang_switch_dropdown():
-    """Render dropdown menu with available languages."""
-    return {}
+def lang_switch_dropdown(redirect_to: str = None):
+    """
+    Render dropdown menu with available languages.
+
+    Args:
+        redirect_to: Where to redirect after language change.
+
+    Returns:
+        Rendered select box language switch.
+
+    """
+    return {'redirect_to': redirect_to}
--- django_lang_switch/tests/test_templatetags.py
+++ django_lang_switch/tests/test_templatetags.py
@@ -21,3 +21,15 @@ class TestDropdownMenu(SimpleTestCase):
         self.assertInHTML('<option value="en">English (en)</option>', rendered)
         self.assertInHTML('<option value="cs" selected>Česky (cs)</option>', rendered)
         self.assertInHTML('<option value="ko">한국어 (ko)</option>', rendered)
+
+    @override_settings(
+        LANGUAGES=[('en', 'English'), ('cs', 'Czech'), ('ko', 'Korean')],
+        LANGUAGE_CODE='cs')
+    def test_redirect_to(self):
+        rendered = self.render_template(
+            '{% load lang_switch %}{% lang_switch_dropdown redirect_to="hogwarts" %}'
+        )
+        self.assertInHTML('<option value="en">English (en)</option>', rendered)
+        self.assertInHTML('<option value="cs" selected>Česky (cs)</option>', rendered)
+        self.assertInHTML('<option value="ko">한국어 (ko)</option>', rendered)
+        self.assertInHTML('<input name="next" type="hidden" value="hogwarts">', rendered)
